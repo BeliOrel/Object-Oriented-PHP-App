a652906765ccdef80af5b617daa16ddd733f6879
Beli Orel <anokasan@gmail.com>
2019-05-21 19:49:26 +0200

    Insert Posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -20,6 +20,44 @@ class Posts extends Controller {
   }
 
   public function add(){
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      // Sanitize the POST array
+      $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+      
+      $data = [
+        'title' => trim($_POST['title']),
+        'body' => trim($_POST['body']),
+        'user_id' => $_SESSION['user_id'],
+        'title_error' => '',
+        'body_error' => ''
+      ];
+
+      // Validate data
+      if(empty($data['title'])){
+        $data['title_error'] = 'Please enter title';
+      }
+      if(empty($data['body'])){
+        $data['body_error'] = 'Please enter body text';
+      }
+
+      // Make sure there's no errors
+      if(empty($data['title_error']) && empty($data['body_error'])){ 
+        // Validated
+        if($this->postModel->addPost($data)){
+          flash('post_message', 'Post added');
+          redirect('posts');
+        } else {
+          die('Something went wrong!');
+        }
+      } else {
+        // Load view with errors
+        $this->view('posts/add', $data);
+      }
+
+    } else {
+
+    }
+
     $data = [
       'title' => '',
       'body' => ''
--- app/views/posts/add.php
+++ app/views/posts/add.php
@@ -3,7 +3,7 @@
     <div class="card card-body bg-light mt-5">
       <h2>Add Post</h2>
       <p>Create a new post</p>
-      <form action="<?php echo URLROOT; ?>/users/login" method="POST">
+      <form action="<?php echo URLROOT; ?>/posts/add" method="POST">
         <div class="form-group">
           <label for="title">Title: <sup>*</sup></label>
           <input type="text" name="title" class="form-control form-control-lg <?php echo (!empty($data['title_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['title']; ?>">
