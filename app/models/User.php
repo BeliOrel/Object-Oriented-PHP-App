3dcf1e53243db27aebc501d55ac20599d6b7b915
Beli Orel <anokasan@gmail.com>
2019-05-21 22:24:44 +0200

    Post show deatils Page

--- app/models/User.php
+++ app/models/User.php
@@ -52,4 +52,15 @@ class User {
       return false;
     }
   }
+
+  // Get User by ID
+  public function getUserById($id) {
+    $this->db->query('SELECT * FROM users WHERE id = :id');
+    // bind value
+    $this->db->bind(':id', $id);
+
+    $row = $this->db->single();
+
+    return $row;
+  }
 }
